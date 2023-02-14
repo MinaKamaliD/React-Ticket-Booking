d76cca5ca36f1593412e4bc4dc1683de43e4e5b6
Mina <minakamali.d@gmail.com>
2023-02-15 01:45:11 +0330

    functional component added

--- src/components/TicketHomePage/TicketHomePage.js
+++ src/components/TicketHomePage/TicketHomePage.js
@@ -3,95 +3,182 @@ import { Component } from "react";
 import { useState } from "react";
 import "./TicketHomePage.css";
 
-///////// Class Component
+/////// Functional Component /////////
 
-class TicketHomePage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      info: {
-        Iran: [
-          "Tehran",
-          "Mashhad",
-          "Isfahan",
-          "Kerman",
-          "Tabriz",
-          "Rasht",
-          "Shiraz",
-          "Zahedan",
-        ],
-        Turkey: [
-          "Istanbul",
-          "Ankara",
-          "Izmir",
-          "Bursa",
-          "Antalya",
-          "Şanlıurfa",
-          "Konya",
-        ],
-        UnitedStates: [
-          "New York",
-          "Los Angeles",
-          "Chicago",
-          "Houston",
-          "Phoenix",
-          "Philadelphia",
-          "San Antonio",
-          "San Diego",
-          "Dallas",
-          "San Jose",
-        ],
-        France: [
-          "Paris",
-          "Nice",
-          "Lyon",
-          "Bordeaux",
-          "Marseilles",
-          "Strasbourg",
-          "Lille",
-        ],
-      },
-      currentCountry: "",
-      currentCities: [],
-    };
-    this.currentCountryHandler = this.currentCountryHandler.bind(this);
-  }
-  currentCountryHandler(event) {
-    let a = event.target.value;
-    this.setState({ currentCountry: event.target.value });
-    this.setState({ currentCities: this.state.info[event.target.value] });
-  }
+const TicketHomePage = () => {
+  const [data, setData] = useState({
+    Iran: [
+      "Tehran",
+      "Mashhad",
+      "Isfahan",
+      "Kerman",
+      "Tabriz",
+      "Rasht",
+      "Shiraz",
+      "Zahedan",
+    ],
+    Turkey: [
+      "Istanbul",
+      "Ankara",
+      "Izmir",
+      "Bursa",
+      "Antalya",
+      "Şanlıurfa",
+      "Konya",
+    ],
+    UnitedStates: [
+      "New York",
+      "Los Angeles",
+      "Chicago",
+      "Houston",
+      "Phoenix",
+      "Philadelphia",
+      "San Antonio",
+      "San Diego",
+      "Dallas",
+      "San Jose",
+    ],
+    France: [
+      "Paris",
+      "Nice",
+      "Lyon",
+      "Bordeaux",
+      "Marseilles",
+      "Strasbourg",
+      "Lille",
+    ],
+  });
 
-  render() {
-    return (
-      <div className="container">
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="number" placeholder="Phone Number" />
-        <input type="email" placeholder="Email" />
-        <input type="date" />
-        <select onChange={this.currentCountryHandler}>
-          <option value="countries" hidden>
-            Country
+  const [currentCities, setCurrentCities] = useState([]);
+  const [currentCountry, setCurrentCountry] = useState("");
+
+  const currentCountryHandler = (event) => {
+    setCurrentCountry(event.target.value);
+    console.log(currentCountry);
+
+    setCurrentCities(data[currentCountry]);
+    console.log([data[currentCountry]]);
+  };
+  return (
+    <div className="container">
+      <input type="text" placeholder="First Name" />
+      <input type="text" placeholder="Last Name" />
+      <input type="number" placeholder="Phone Number" />
+      <input type="email" placeholder="Email" />
+      <input type="date" />
+      <select onChange={currentCountryHandler}>
+        <option value="countries" hidden>
+          Country
+        </option>
+        <option value="Iran">Iran</option>
+        <option value="Turkey">Turkey</option>
+        <option value="UnitedStates">United States</option>
+        <option value="France">France</option>
+      </select>
+      <select>
+        <option value="city" hidden>
+          City
+        </option>
+        {currentCities.map((city) => (
+          <option key={city} value={city}>
+            {city}
           </option>
-          <option value="Iran">Iran</option>
-          <option value="Turkey">Turkey</option>
-          <option value="UnitedStates">United States</option>
-          <option value="France">France</option>
-        </select>
-        <select>
-          <option value="city" hidden>
-            City
-          </option>
-          {this.state.currentCities.map((city) => (
-            <option key={city} value={city}>
-              {city}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
-  }
-}
-
+        ))}
+      </select>
+    </div>
+  );
+};
 export default TicketHomePage;
+
+///////// Class Component ////////
+
+// class TicketHomePage extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       info: {
+//         Iran: [
+//           "Tehran",
+//           "Mashhad",
+//           "Isfahan",
+//           "Kerman",
+//           "Tabriz",
+//           "Rasht",
+//           "Shiraz",
+//           "Zahedan",
+//         ],
+//         Turkey: [
+//           "Istanbul",
+//           "Ankara",
+//           "Izmir",
+//           "Bursa",
+//           "Antalya",
+//           "Şanlıurfa",
+//           "Konya",
+//         ],
+//         UnitedStates: [
+//           "New York",
+//           "Los Angeles",
+//           "Chicago",
+//           "Houston",
+//           "Phoenix",
+//           "Philadelphia",
+//           "San Antonio",
+//           "San Diego",
+//           "Dallas",
+//           "San Jose",
+//         ],
+//         France: [
+//           "Paris",
+//           "Nice",
+//           "Lyon",
+//           "Bordeaux",
+//           "Marseilles",
+//           "Strasbourg",
+//           "Lille",
+//         ],
+//       },
+//       currentCountry: "",
+//       currentCities: [],
+//     };
+//     this.currentCountryHandler = this.currentCountryHandler.bind(this);
+//   }
+//   currentCountryHandler(event) {
+//     let a = event.target.value;
+//     this.setState({ currentCountry: event.target.value });
+//     this.setState({ currentCities: this.state.info[event.target.value] });
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <input type="text" placeholder="First Name" />
+//         <input type="text" placeholder="Last Name" />
+//         <input type="number" placeholder="Phone Number" />
+//         <input type="email" placeholder="Email" />
+//         <input type="date" />
+//         <select onChange={this.currentCountryHandler}>
+//           <option value="countries" hidden>
+//             Country
+//           </option>
+//           <option value="Iran">Iran</option>
+//           <option value="Turkey">Turkey</option>
+//           <option value="UnitedStates">United States</option>
+//           <option value="France">France</option>
+//         </select>
+//         <select>
+//           <option value="city" hidden>
+//             City
+//           </option>
+//           {this.state.currentCities.map((city) => (
+//             <option key={city} value={city}>
+//               {city}
+//             </option>
+//           ))}
+//         </select>
+//       </div>
+//     );
+//   }
+// }
+
+// export default TicketHomePage;
